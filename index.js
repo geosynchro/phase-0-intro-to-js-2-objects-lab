3e28b61dfe1bcbd4d4168c180671b6f3c3ba13bd
Nicholas Fagner <nicholasfagner@gmail.com>
2021-10-01 13:12:08 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+const employee ={
+    name: "Nick",
+    streetAddress: "1604 Utica",
+};
+
+function updateEmployeeWithKeyAndValue(obj , key, value){
+    return {...obj, [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function  deleteFromEmployeeByKey(obj, key){
+    const newEmployee = {...obj}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    const newEmployee = obj
+    delete newEmployee[key]
+    return newEmployee
+}
+  
+  
+
+
+   
+
+
+
+
